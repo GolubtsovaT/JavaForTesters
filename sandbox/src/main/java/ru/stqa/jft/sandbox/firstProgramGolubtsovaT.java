f6dafa78bf36c83c41f05ad71033fb5d16626148
GolubtsovaT <tatyana.golubtsova.103@gmail.com>
2021-08-20 16:34:08 +0300

    Created package for firstProgramGolubtsovaT

--- sandbox/src/main/java/ru/stqa/jft/sandbox/firstProgramGolubtsovaT.java
+++ sandbox/src/main/java/ru/stqa/jft/sandbox/firstProgramGolubtsovaT.java
@@ -1,4 +1,6 @@
-public class FirstProgramGolubtsovaT {
+package ru.stqa.jft.sandbox;
+
+public class firstProgramGolubtsovaT {
 	
 	public static void main (String[] args) {
 System.out.println("Hello, world!");
