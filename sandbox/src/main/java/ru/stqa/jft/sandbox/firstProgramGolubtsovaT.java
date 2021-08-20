ef4a27d763873dae6e9d4e44fd1bf9e327dddf1d
GolubtsovaT <tatyana.golubtsova.103@gmail.com>
2021-08-20 16:55:27 +0300

    Code reformated

--- sandbox/src/main/java/ru/stqa/jft/sandbox/firstProgramGolubtsovaT.java
+++ sandbox/src/main/java/ru/stqa/jft/sandbox/firstProgramGolubtsovaT.java
@@ -1,9 +1,9 @@
 package ru.stqa.jft.sandbox;
 
 public class firstProgramGolubtsovaT {
-	
-	public static void main (String[] args) {
-System.out.println("Hello, world!");
-	}
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 
 }
