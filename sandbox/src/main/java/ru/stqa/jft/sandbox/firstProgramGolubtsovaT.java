6cf1f051eb74c469df5cc2892f5decf13ae05a00
GolubtsovaT <tatyana.golubtsova.103@gmail.com>
2021-08-20 22:46:19 +0300

    Changes in new classes: Functions to Methods

--- sandbox/src/main/java/ru/stqa/jft/sandbox/firstProgramGolubtsovaT.java
+++ sandbox/src/main/java/ru/stqa/jft/sandbox/firstProgramGolubtsovaT.java
@@ -7,22 +7,14 @@ public class firstProgramGolubtsovaT {
         hello("user");
 
         Square s = new Square(5);
-        System.out.println("Square area with side " + s.l + " = " + area(s));
+        System.out.println("Square area with side " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(4,6);
-        System.out.println("Rectangle area with sides " + r.a + " and " + r.b + " = " + area(r) );
+        System.out.println("Rectangle area with sides " + r.a + " and " + r.b + " = " + r.area() );
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody+ "!");
-    }
-
-    public static double area (Square s) {
-        return s.l * s.l;
-    }
-
-    public static double area (Rectangle r) {
-        return r.a * r.b;
     }
 
 }
