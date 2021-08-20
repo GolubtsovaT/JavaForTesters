d0ab1a9c3fd9cc5b635dc0b1c50902cd3e978b81
GolubtsovaT <tatyana.golubtsova.103@gmail.com>
2021-08-20 22:37:22 +0300

    New classes added

--- sandbox/src/main/java/ru/stqa/jft/sandbox/firstProgramGolubtsovaT.java
+++ sandbox/src/main/java/ru/stqa/jft/sandbox/firstProgramGolubtsovaT.java
@@ -3,7 +3,26 @@ package ru.stqa.jft.sandbox;
 public class firstProgramGolubtsovaT {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        hello("world");
+        hello("user");
+
+        Square s = new Square(5);
+        System.out.println("Square area with side " + s.l + " = " + area(s));
+
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Rectangle area with sides " + r.a + " and " + r.b + " = " + area(r) );
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody+ "!");
+    }
+
+    public static double area (Square s) {
+        return s.l * s.l;
+    }
+
+    public static double area (Rectangle r) {
+        return r.a * r.b;
     }
 
 }
