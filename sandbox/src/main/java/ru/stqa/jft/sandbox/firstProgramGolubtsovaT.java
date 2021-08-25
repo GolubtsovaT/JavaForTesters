d644e35c418ef2e3a6838fb8724ee14a6afaa949
GolubtsovaT <tatyana.golubtsova.103@gmail.com>
2021-08-26 00:32:17 +0300

    Task 2. Added class Point; Deleted test classes Square and Rectangle

--- sandbox/src/main/java/ru/stqa/jft/sandbox/firstProgramGolubtsovaT.java
+++ sandbox/src/main/java/ru/stqa/jft/sandbox/firstProgramGolubtsovaT.java
@@ -3,18 +3,12 @@ package ru.stqa.jft.sandbox;
 public class firstProgramGolubtsovaT {
 
     public static void main(String[] args) {
-        hello("world");
-        hello("user");
-
-        Square s = new Square(5);
-        System.out.println("Square area with side " + s.l + " = " + s.area());
-
-        Rectangle r = new Rectangle(4,6);
-        System.out.println("Rectangle area with sides " + r.a + " and " + r.b + " = " + r.area() );
+        Point p1 = new Point (9.7,2.6);
+        Point p2 = new Point (16.8,-7.4);
+        System.out.println("Distance between points p1 and p2 = " + distance(p1,p2));
     }
 
-    public static void hello(String somebody) {
-        System.out.println("Hello, " + somebody+ "!");
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
     }
-
 }
