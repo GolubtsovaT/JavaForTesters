a96c1c3b26ff38bed6d20f645f81b038ca8ec71d
GolubtsovaT <tatyana.golubtsova.103@gmail.com>
2021-08-26 01:45:28 +0300

    Task 2. Added method distance

--- sandbox/src/main/java/ru/stqa/jft/sandbox/firstProgramGolubtsovaT.java
+++ sandbox/src/main/java/ru/stqa/jft/sandbox/firstProgramGolubtsovaT.java
@@ -3,9 +3,11 @@ package ru.stqa.jft.sandbox;
 public class firstProgramGolubtsovaT {
 
     public static void main(String[] args) {
-        Point p1 = new Point (9.7,2.6);
-        Point p2 = new Point (16.8,-7.4);
+        Point p1 = new Point (-11.7,6.6);
+        Point p2 = new Point (8,-1.4);
         System.out.println("Distance between points p1 and p2 = " + distance(p1,p2));
+
+        System.out.println("Distance between points p1 and p2 = " + p1.distance(p2));
     }
 
     public static double distance(Point p1, Point p2) {
